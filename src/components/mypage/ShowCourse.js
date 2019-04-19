fbf148ffb83e25b9d6c05c4963d431f3efca4f8d
kouhei ueno <k.cola119@gmail.com>
2019-04-19 19:20:49 +0900

    route表示

--- src/components/mypage/ShowCourse.js
+++ src/components/mypage/ShowCourse.js
@@ -11,7 +11,8 @@ class ShowCourse extends React.Component {
         console.log(this.courseInfo)
         this.state = {
             selectedPath: [],
-            selectedCircles: []
+            selectedCircles: [],
+            selectedPointsOfRoute: [],
         };
     }
 
@@ -27,7 +28,14 @@ class ShowCourse extends React.Component {
         this.setState({ selectedPath: [this.courseInfo.paths[id]], selectedCircles: selectedCircles });
     }
 
+    showRoute = (route) => {
+        // console.log(route)
+        this.setState({ selectedCircles: route.haveCircles, selectedPath: route.havePath })
+        this.setState({ selectedPointsOfRoute: route.points })
+    }
+
     render() {
+        this.state.selectedPointsOfRoute.length > 0 && console.log([{ points: [this.state.selectedCircles[0].id, ...this.state.selectedPointsOfRoute.map(v => v.id), this.state.selectedCircles[this.state.selectedCircles.length - 1].id] }])
         return (
             <div>
                 <div>
@@ -55,9 +63,31 @@ class ShowCourse extends React.Component {
                             r={this.courseInfo.circleStyle.r}
                             strokeWidth={this.courseInfo.circleStyle.strokeWidth}
                             opacity={this.courseInfo.circleStyle.opacity}
-                            event={{}}
-                        />
+                            event={{}} />
+                        {this.state.selectedPointsOfRoute.length > 0 &&
+                            <CirclesAndPaths
+                                circles={[...this.state.selectedCircles, ...this.state.selectedPointsOfRoute]}
+                                paths={[{ points: [this.state.selectedCircles[0].id, ...this.state.selectedPointsOfRoute.map(v => v.id), this.state.selectedCircles[this.state.selectedCircles.length - 1].id] }]}
+                                r={0}
+                                strokeWidth={3}
+                                opacity={0}
+                                text={""}
+                                event={{}} />}
                     </SVGViewArea>
+                </div>
+
+                <div>
+                    {this.courseInfo.haveRoutes.map((haveRoute, index) => (
+                        <div key={index}>
+                            {index}.{haveRoute.routesName}
+                            {haveRoute.routes.map((route, i) => (
+                                <div key={i}>
+                                    {i} . {route.routeName} {route.points.length}コントロール
+                                    <button className="btn" onClick={(_) => this.showRoute(route)} id={index}>view</button>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </div>
         );
